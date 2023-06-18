6a92ba8aa668fe1ace44a8a3e87982f3b5a602f6
AgrawalKashish <96725005+AgrawalKashish@users.noreply.github.com>
2023-06-18 12:12:58 +0530

    search_bar_edits

--- exp_history.js
+++ exp_history.js
@@ -280,3 +280,25 @@ function printData()
    newWin.print();
    newWin.close();
 }
+
+
+//search
+function myFunction_() {
+ 
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("input_my");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("table_my");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
